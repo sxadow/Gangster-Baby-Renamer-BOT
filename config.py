df6d66cf71e08efd91df18aff099fabdac96dadb
sxadow <107715054+sxadow@users.noreply.github.com>
2022-12-21 16:28:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,15 +8,11 @@ API_HASH = os.environ.get("API_HASH", "fdafb41f9a67f40e34a6c67f47730a92")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "5635001118:AAEZdFNiL1ICi5J0WN6v-97ON5MLArBQO0o")
 
-
-
-FORCE_SUB = os.environ.get("FORCE_SUB", "tronmovies
-
-") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "TVHD10") 
 
 DB_NAME = os.environ.get("DB_NAME","Cluster0")     
 
-DB_URL = os.environ.get("DB_URL","mongodb+srv://tron:tron@cluster0.xs0gvih.mongodb.net/?retryWrites=true&w=majority")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://tronn:tronn@cluster0.g9hwdft.mongodb.net/?retryWrites=true&w=majority")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
